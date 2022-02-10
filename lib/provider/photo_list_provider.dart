b5d0d84bf20206f3aaec5de899eb0d9ddbb3c878
GunjonRoy <01gunjonroy5722@gmail.com>
2022-02-10 23:11:06 +0600

    final

--- lib/provider/photo_list_provider.dart
+++ lib/provider/photo_list_provider.dart
@@ -1,27 +1,28 @@
 
 import 'package:flutter/cupertino.dart';
 import '../model/Helper/photo_load_helper.dart';
-import '../model/core/photoDataModel.dart';
 
 
 class PhotoListProvider with ChangeNotifier{
 
   PhotoLoadHelper photoLoadHelper=PhotoLoadHelper();
-
-
   List<String> loadedPhotos=[];
 
 
   loadTwentyPhotos()async{
     //loadedPhotos=List.from(loadedPhotos)..addAll(photoLoadHelper.loadPhotosList()as List<PhotoDataModel>);
     loadedPhotos = [...await photoLoadHelper.loadTenPhotosList()];
+    for(int i=0;i<loadedPhotos.length;i++){
+      print("PhotoListProvider"+i.toString());
+      print(loadedPhotos[i].toString());
+    }
     notifyListeners();
-    return loadedPhotos;
+    //return loadedPhotos;
   }
 
 
 
-  get getLoadedPhotoList{
+  get getLoadedPhotoListLength{
     return loadedPhotos;
   }
 
